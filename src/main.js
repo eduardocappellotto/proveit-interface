e974666b25c063280060a1d988e799ea67a6732b
Eduardo <eduardocappellotto@gmail.com>
2023-08-25 13:33:21 -0300

    feat: baseline, adiciona rotas, vuetify e store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+import router from './routes';
+import store from './store';
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
+  store,
   render: h => h(App),
+  router,
 }).$mount('#app')
