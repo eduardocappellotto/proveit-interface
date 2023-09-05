47e0fdcff37f20eaebdaf7be89ca7f1e4ed4b3e7
Eduardo <eduardocappellotto@gmail.com>
2023-09-05 03:50:13 -0300

    feat: adiciona baseUrl da railway

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import store from '@/store'
 import router from '@/routes'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://proveit-server-production.up.railway.app/',
 });
 
 instance.interceptors.request.use((config) => {
